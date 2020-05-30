faae39b5c293417773e0fafba6a2c7840fbcfbc8
Kenan Sikiric <kensi528@student.liu.se>
2020-05-30 21:15:47 +0200

    changed design and structure of variable cards

--- py_explorer.py
+++ py_explorer.py
@@ -10,11 +10,21 @@ def update_table(doc, global_vars):
         row.remove()
     for k,v in global_vars["editor_ns"].items():
         card_div = html.DIV(Class="card")
-        card_header = html.DIV(id=f"h_{k}", Class="card-header")
-        card_header <= html.BUTTON(k, Class="card_font btn btn-link btn-block text-left", **{"data-toggle":"collapse", "data-target":f"#c_{k}", "aria-expanded":"false", "aria-controls":f"c_{k}"}) 
+        card_header = html.DIV(id=f"h_{k}", Class="card-header", **{"data-toggle":"collapse", "data-target":f"#c_{k}", "aria-expanded":"false", "aria-controls":f"c_{k}"})
+        
+        card_header_varname = html.SPAN("", Class="card_header_inner")
+        card_header_varvalue = html.SPAN("", Class="card_header_inner card_header_inner_value")
+        
+        card_header_varname <= html.P(k, Class="card_font card_header_text") 
+        card_header_varvalue <= html.P(repr(v), Class="card_font card_header_text", **{"data-toggle":"collapse", "data-target":f"#c_{k}", "aria-expanded":"false", "aria-controls":f"c_{k}"}) 
+        
+        card_header <= card_header_varname
+        card_header <= card_header_varvalue
         card_div <= card_header
 
-        card_content = html.DIV(id=f"c_{k}", Class="collapse hide", **{"aria-labelledby":f"h_{k}", "data-parent":"#var_accordion"})
-        card_content <= html.DIV(repr(v), Class="card-body card_font")
+        card_content = html.DIV(id=f"c_{k}", Class="collapse hide card_content_root", **{"aria-labelledby":f"h_{k}", "data-parent":"#var_accordion"})
+        card_content <= html.DIV("[object information table to be added]", Class="card-body card_font")
+        
+        
         card_div <= card_content
         doc["var_accordion"] <= card_div
